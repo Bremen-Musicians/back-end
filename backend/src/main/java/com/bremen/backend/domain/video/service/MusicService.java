7f764170f8d1fc56a76b6df9bbf08ecb2e9fa403
onyoo <dhsdb02@gmail.com>
2024-10-23 23:40:55 +0900

    refactor: #183 음악 관련 응답객체 수정

--- backend/src/main/java/com/bremen/backend/domain/video/service/MusicService.java
+++ backend/src/main/java/com/bremen/backend/domain/video/service/MusicService.java
@@ -1,12 +1,13 @@
 package com.bremen.backend.domain.video.service;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.bremen.backend.domain.video.dto.MusicResponse;
 import com.bremen.backend.domain.video.entity.Music;
-import com.bremen.backend.global.response.ListResponse;
 
 public interface MusicService {
 	Music getMusicById(Long musicId);
 
-	ListResponse searchMusicsByTitle(String title, Pageable pageable);
+	Page<MusicResponse> searchMusicsByTitle(String title, Pageable pageable);
 }

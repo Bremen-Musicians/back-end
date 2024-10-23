a912cf89fb0f9396709ad13d218e32e567e23d32
onyoo <dhsdb02@gmail.com>
2024-10-23 23:40:43 +0900

    refactor: #183 유저 관련 응답객체 수정

--- backend/src/main/java/com/bremen/backend/domain/user/controller/FollowController.java
+++ backend/src/main/java/com/bremen/backend/domain/user/controller/FollowController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bremen.backend.domain.user.service.FollowUserService;
-import com.bremen.backend.global.response.SingleResponse;
+import com.bremen.backend.global.response.Response;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,11 +23,11 @@ public class FollowController {
 
 	@GetMapping("/follow")
 	@Operation(summary = "해당 ID를 가진 사람을 팔로우합니다.")
-	public ResponseEntity<SingleResponse<String>> follow(@RequestParam(value = "nickname") String nickname) {
+	public ResponseEntity<Response<String>> follow(@RequestParam(value = "nickname") String nickname) {
 		if (followUserService.followUser(nickname)) {
-			return ResponseEntity.ok(new SingleResponse<>(HttpStatus.OK.value(), "팔로우 성공", ""));
+			return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), "팔로우 성공", ""));
 		} else {
-			return ResponseEntity.ok(new SingleResponse<>(HttpStatus.OK.value(), "언팔로우 성공", ""));
+			return ResponseEntity.ok(new Response<>(HttpStatus.OK.value(), "언팔로우 성공", ""));
 		}
 
 	}

206e04048fc2050a99dc4c30962c18d401c4df3f
onyoo <dhsdb02@gmail.com>
2024-10-25 23:19:34 +0900

    fix(auth): #186 유저 인증정보 게터 추가

--- backend/src/main/java/com/bremen/backend/domain/user/entity/PrincipalDetails.java
+++ backend/src/main/java/com/bremen/backend/domain/user/entity/PrincipalDetails.java
@@ -6,6 +6,9 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import lombok.Getter;
+
+@Getter
 public class PrincipalDetails implements UserDetails {
 	private User user;
 
